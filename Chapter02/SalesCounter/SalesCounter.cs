3938547f85aabeb399ce9bafb9b6a7d21b39ff12
KobayashiRio <166583938+35144KobayashiRio@users.noreply.github.com>
2024-04-24 14:12:49 +0900

    メソッド移動

--- Chapter02/SalesCounter/SalesCounter.cs
+++ Chapter02/SalesCounter/SalesCounter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,18 +11,33 @@ namespace SalesCounter {
         private List<Sale> _sales;
 
         //コンストラクタ
-        public SalesCounter(List<Sale> sales) {
-            _sales = sales;
+        public SalesCounter(string filePass) {
+            _sales = ReadSales(filePass);
+        }
 
+        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
+        static List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
         }
 
         //店舗別の売り上げを求める
-        public Dictionary<string,int> GetPerStoreSales() {
-            Dictionary<string,int>dict=new Dictionary<string,int>();
+        public Dictionary<string, int> GetPerStoreSales() {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
             foreach (Sale sale in _sales) {
-                if(dict.ContainsKey(sale.ShopName)){
+                if (dict.ContainsKey(sale.ShopName)) {
                     dict[sale.ShopName] += sale.Amount;
-                }else{
+                } else {
                     dict[sale.ShopName] = sale.Amount;
                 }
             }
