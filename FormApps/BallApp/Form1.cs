be96bf7b83e4deb882c13be124b311f513037cf3
KobayashiRio <166583938+35144KobayashiRio@users.noreply.github.com>
2024-05-08 15:59:43 +0900

    完成

--- FormApps/BallApp/Form1.cs
+++ FormApps/BallApp/Form1.cs
@@ -2,45 +2,58 @@ using System.Diagnostics.Eventing.Reader;
 
 namespace BallApp {
     public partial class Form1 : Form {
+        //Obj ball;
+        //PictureBox pb;
 
-        private double posX;   //X座標
-        private double posY;   //Y座標
-        private double moveX;  //移動量(X方向)
-        private double moveY;  //移動量(Y方向)
-
-
+        //Listコレクション
+        private List<Obj>balls=new List<Obj>();    //ボールインスタンス格納用
+        private List<PictureBox> pbs=new List<PictureBox>();   //表示用
+       
 
         //コンストラクタ
         public Form1() {
             InitializeComponent();
 
-            moveX = moveY = 3;
         }
 
         //フォームが最初にロードされるとき一度だけ実行される
         private void Form1_Load(object sender, EventArgs e) {
 
-            //this.BackColor = Color.Green;
-            timer1.Start();
         }
 
+        
         private void timer1_Tick(object sender, EventArgs e) {
+           
 
-            //現在位置を表示
-            this.Text = pbBall.Location.ToString();
+            for(int i = 0; i<balls.Count; i++) {
+                balls[i].Move();
+                pbs[i].Location = new Point((int)balls[i].PosX, (int)balls[i].PosY);
 
-            if (pbBall.Location.X > 750 || pbBall.Location.X < 0) {
-                moveX = -moveX;
+                }
             }
 
-            if (pbBall.Location.Y > 500 || pbBall.Location.Y < 0) {
-                moveY = -moveY;
+        private void Form1_MouseClick(object sender, MouseEventArgs e) {
+            PictureBox pb = new PictureBox();  //画像を表示するコントロール
+            Obj ball = null ;  
+
+            if (e.Button == MouseButtons.Left) {
+                ball = new SoccerBall(e.X - 25, e.Y - 25);
+                pb.Size = new Size(50, 50);
+
+            } else if (e.Button == MouseButtons.Right) {
+                ball = new TennisBall(e.X - 12, e.Y - 12);
+                pb.Size = new Size(25, 25);
             }
+                pb.Image = ball.Image;
+                pb.Location = new Point((int)ball.PosX, (int)ball.PosY);
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                pb.Parent = this;
 
-            posX += moveX;
-            posY += moveY;
+                timer1.Start();
 
-            pbBall.Location = new Point((int)posX, (int)posY);
+            balls.Add(ball);
+            pbs.Add(pb);
+
+            }
         }
     }
-}
---
+++ FormApps/BallApp/SoccerBall.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallApp {
+    internal class SoccerBall : Obj {
+        
+
+        public SoccerBall(double xp, double yp)
+            : base(xp-25, yp-25, @"Properties\Picture\soccer_ball.png") {
+
+            MoveX = 10; //移動量設定
+            MoveY = 10;
+        }
+
+        public override bool Move() {
+
+            if (PosX > 750 || PosX < 0) {
+                MoveX = -MoveX;
+            }
+
+            if (PosY > 500 || PosY < 0) {
+                MoveY = -MoveY;
+            }
+
+
+            PosX += MoveX;
+            PosY += MoveY;
+
+            return true;
+        }
+    }
+}
