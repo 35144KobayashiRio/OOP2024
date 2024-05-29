e9ccdf20dc50c47f6229924144cc05087d9d587b
KobayashiRio <166583938+35144KobayashiRio@users.noreply.github.com>
2024-05-29 15:58:39 +0900

    演習問題５－１完成

---
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var str1 = Console.ReadLine();
+            var str2 = Console.ReadLine();
+
+            if (String.Compare(str1,str2,true)==0) {
+                Console.WriteLine("一致する");
+            } else {
+                Console.WriteLine("一致しない");
+            }
+        }
+    }
+}
