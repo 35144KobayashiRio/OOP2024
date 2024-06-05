f06b4226161053053b5dd2dc808679d01a9e0dd9
KobayashiRio <166583938+35144KobayashiRio@users.noreply.github.com>
2024-06-05 13:15:35 +0900

    第6章演習問題

---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,20 @@
+﻿using Chapter06;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var books = Books.GetBooks().OrderByDescending(b => b.Price).ToList();
+            books.ForEach(b => Console.WriteLine(b.Title + ":" + b.Price));
+
+
+            //foreach (var book in books) {
+            //Console.WriteLine(book.Title+":"+book.Price);}
+
+        }
+    }
+}
