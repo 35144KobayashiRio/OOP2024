1e93d71e258c425c3e7181633d211f5224236fb4
KobayashiRio <166583938+35144KobayashiRio@users.noreply.github.com>
2024-06-06 13:42:45 +0900

    第６章演習問題完成

--- Chapter06/演習問題/Exercise02/Exercise02/Book.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Exercise02 {
-    internal class Book {
-        public string Title { get; set; }
-        public int Price { get; set; }
-        public int Pages { get; set; }
-    }
-}
--- Chapter06/演習問題/Exercise02/Exercise02/Program.cs
+++ Chapter06/演習問題/Exercise02/Exercise02/Program.cs
@@ -1,21 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace Exercise02 {
+    class Book {
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public int Pages { get; set; }
+    }
+
     internal class Program {
         static void Main(string[] args) {
+
             var books = new List<Book> {
-   new Book { Title = "C#プログラミングの新常識", Price = 3800, Pages = 378 },
-   new Book { Title = "ラムダ式とLINQの極意", Price = 2500, Pages = 312 },
-   new Book { Title = "ワンダフル・C#ライフ", Price = 2900, Pages = 385 },
-   new Book { Title = "一人で学ぶ並列処理プログラミング", Price = 4800, Pages = 464 },
-   new Book { Title = "フレーズで覚えるC#入門", Price = 5300, Pages = 604 },
-   new Book { Title = "私でも分かったASP.NET MVC", Price = 3200, Pages = 453 },
-   new Book { Title = "楽しいC#プログラミング教室", Price = 2540, Pages = 348 },
-};
+               new Book { Title = "C#プログラミングの新常識", Price = 3800, Pages = 378 },
+               new Book { Title = "ラムダ式とLINQの極意", Price = 2500, Pages = 312 },
+               new Book { Title = "ワンダフル・C#ライフ", Price = 2900, Pages = 385 },
+               new Book { Title = "一人で学ぶ並列処理プログラミング", Price = 4800, Pages = 464 },
+               new Book { Title = "フレーズで覚えるC#入門", Price = 5300, Pages = 604 },
+               new Book { Title = "私でも分かったASP.NET MVC", Price = 3200, Pages = 453 },
+               new Book { Title = "楽しいC#プログラミング教室", Price = 2540, Pages = 348 },
+            };
 
             Exercise2_1(books);
             Console.WriteLine("-----");
@@ -43,24 +53,47 @@ namespace Exercise02 {
 
 
         private static void Exercise2_1(List<Book> books) {
+            var books2 = books.Where(b => b.Title == "ワンダフル・C#ライフ");
+            foreach (var book in books2) {
+                Console.WriteLine("{0}:{1}円{2}ページ", book.Title, book.Price, book.Pages);
+            }
         }
 
         private static void Exercise2_2(List<Book> books) {
+            int count = books.Count(b => b.Title.Contains("C#"));
+            Console.WriteLine(count);
         }
 
         private static void Exercise2_3(List<Book> books) {
+            var average = books.Where(b => b.Title.Contains("C#")).Average(b=>b.Pages);
+            Console.WriteLine(average);
+
         }
 
         private static void Exercise2_4(List<Book> books) {
+            var book = books.FirstOrDefault(b => b.Price >= 4000);
+            if(book!=null)
+                Console.WriteLine(book.Title);
         }
 
         private static void Exercise2_5(List<Book> books) {
+            var pages=books.Where(b=>b.Price<4000)
+                           .Max(b=>b.Pages);
+            Console.WriteLine(pages);
         }
 
         private static void Exercise2_6(List<Book> books) {
+            var books3 = books.Where(b => b.Pages >= 400).OrderByDescending(b => b.Price);
+            foreach (Book book in books3) 
+                Console.WriteLine("{0}:{1}円",book.Title , book.Price);
+            
         }
 
         private static void Exercise2_7(List<Book> books) {
+            var books4 = books.Where(b => b.Title.Contains("C#") && b.Pages <= 500);
+            foreach (Book book in books4) 
+                Console.WriteLine(book.Title);
+            
         }
     }
 }
