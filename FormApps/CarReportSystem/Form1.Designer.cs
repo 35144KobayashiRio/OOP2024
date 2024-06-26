a98e3067a918b39c969804b9715c666f9de7bc36
KobayashiRio <166583938+35144KobayashiRio@users.noreply.github.com>
2024-06-26 16:00:18 +0900

    カーレポートクラス

--- FormApps/CarReportSystem/Form1.Designer.cs
+++ FormApps/CarReportSystem/Form1.Designer.cs
@@ -267,6 +267,7 @@
             btAddReport.TabIndex = 14;
             btAddReport.Text = "追加";
             btAddReport.UseVisualStyleBackColor = true;
+            btAddReport.Click += btAddReport_Click;
             // 
             // btModifyReport
             // 
--- FormApps/CarReportSystem/Form1.cs
+++ FormApps/CarReportSystem/Form1.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel;
+
 namespace CarReportSystem {
     public partial class Form1 : Form {
+       
+        //カーレポート管理用リスト
+        BindingList<CarReport>listCarReports=new BindingList<CarReport>();
+
+        //コンストラクタ
         public Form1() {
             InitializeComponent();
+            dgvCarReport.DataSource = listCarReports;
         }
 
         private void label1_Click(object sender, EventArgs e) {
@@ -17,6 +25,15 @@ namespace CarReportSystem {
         }
 
         private void btReportOpen_Click(object sender, EventArgs e) {
+
+        }
+
+        //画像(追加)
+        private void btAddReport_Click(object sender, EventArgs e) {
+            CarReport carReport = new CarReport {
+                Date = dtpDate.Value,
+            };
+            listCarReports.Add(carReport);
 
         }
     }
