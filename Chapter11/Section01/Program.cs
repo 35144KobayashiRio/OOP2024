ce8a6c934d0238cf11b63a156a90bc3d6e6c08a0
KobayashiRio <166583938+35144KobayashiRio@users.noreply.github.com>
2024-07-17 14:57:26 +0900

    P284まで終了

---
+++ Chapter11/Section01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var xdoc = XDocument.Load("novelists.xml");
+            var Xelements = xdoc.Root.Descendants("title");
+
+
+
+            //foreach (var xnovelist in Xelements) {
+            //    var xname = xnovelist.Element("name");             //要素の取得
+            //    var xworks = xnovelist.Element("masterpieces")
+            //        .Elements("title").Select(x => x.Value);              //属性の取得
+            //    Console.WriteLine("{0}-{1}", xname.Value, string.Join(",", xworks));
+            //    //Console.WriteLine($"{xname.Value} 【{xkana?.Value}】{birth.ToShortDateString()}");
+
+            }
+        }
+    }
+}
