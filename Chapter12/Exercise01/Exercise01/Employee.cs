dcdc249728114070ad747ef09531835f1148f027
KobayashiRio <166583938+35144KobayashiRio@users.noreply.github.com>
2024-08-28 15:56:32 +0900

    キー名のカスタマイズ

--- Chapter12/Exercise01/Exercise01/Employee.cs
+++ Chapter12/Exercise01/Exercise01/Employee.cs
@@ -4,8 +4,8 @@ using System.Text.Json.Serialization;
 public class Employee {
     [JsonIgnore]
     public int Id { get; set; }
-    [JsonPropertyName("name")]
+
     public string Name { get; set; }
-    [JsonPropertyName("hireDate")]
+
     public DateTime HireDate { get; set; }
 }
