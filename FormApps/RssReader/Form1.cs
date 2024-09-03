8c9b1e3263bf62ef1a393106ea38a6e8a975679b
KobayashiRio <166583938+35144KobayashiRio@users.noreply.github.com>
2024-09-03 12:08:44 +0900

    RSS 途中

---
+++ FormApps/RssReader/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace RssReader {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btGet_Click(object sender, EventArgs e) {
+            using (var wc = new WebClient()) {
+                var url = wc.OpenRead(tbRssUrl.Text);
+                var xdoc = XDocument.Load(url);
+
+                var xtitle = xdoc.Root.Descendants("item")
+                                     .Select(item => item.Element("title").Value);
+                foreach (var item in xtitle) {
+                    lbRssTitle.Items.Add(item);
+
+                }
+            }
+        }
+    }
+}
